f43de6b2747806ec7c2609ab376a9abc09882fb1
Voichita Rusu <voichita.rusu@iquestgroup.com>
2018-11-16 08:08:18 +0200

    First commit

---
+++ iQuest.FedEx2018.Wims/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace iQuest.FedEx2018.Wims
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
